cc7e38a66dd7146177b7fce470ef460ddf1de967
CodingAmateur12 <116116142+CodingAmateur12@users.noreply.github.com>
2024-03-01 15:58:13 +0000

    2

--- app/src/main/java/com/example/todocompose/data/models/ToDoDao.kt
+++ app/src/main/java/com/example/todocompose/data/models/ToDoDao.kt
@@ -1,8 +1,12 @@
 package com.example.todocompose.data.models
 
+import android.app.appsearch.exceptions.AppSearchException
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 //In DAO you create functions such as update, delete and other.
 @Dao
@@ -27,6 +31,35 @@ interface ToDoDao {
 
     //all functions that don't use Flow need to use suspend key
     //adds new task to database
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addTask(toDoTask: ToDoTask)
+
+    //updating task
+    @Update
+    suspend fun updateTask(toDoTask: ToDoTask)
+
+    //deleting specific task
+    @Delete
+    suspend fun deleteTask(toDoTask: ToDoTask)
+
+    //deleting all tasks
+    @Query("DELETE FROM todo_table")
+    suspend fun deleteAllTasks()
+
+    //you can search you database based on description or title in database
+    @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+
+    //Selecting whole database from todoDatabase then sorting it by priority.
+    //sorting priority colum where First is L in the word LOW, then
+    //M in the priority MEDIUM and THEN H in HIGH. Then finally all other tasks
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
+    fun sortByLowPriority(): Flow<List<ToDoTask>>
+
+
+    //from highest to lowest
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
+    fun sortByHighPriority(): Flow<List<ToDoTask>>
+
+
 }
---
+++ app/src/main/java/com/example/todocompose/data/models/ToDoDatabase.kt
@@ -0,0 +1,24 @@
+package com.example.todocompose.data.models
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+//abstract class are like vehicles. for exam[ple. you want to specify that all vehicles should move and have colour.
+//You create abstract class vehicle
+// Then you create abstract fun move() but dont specify it
+// Then you create abstract var colour but dont specify it what exactly it is.
+//so you can specify it later
+
+//specify all of your entities or all of your data tables that you have
+// in this case we only have one entity or 1 table  ToDoTask
+//version of database which is 1 because we just created this databse
+//version: This parameter specifies the version number of the database. Whenever you change the schema of your database, such as adding or
+// removing tables or columns, you increment this number. It helps Room to handle database migrations properly.
+//
+@Database(entities = [ToDoTask::class], version =1 , exportSchema = false)
+abstract class ToDoDatabase: RoomDatabase()  {
+
+    //returns toDoDao interface 
+    abstract fun toDoDao(): ToDoDao
+}
